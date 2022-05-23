c079207873c0a2b045b855fb3348c6c93751a558
main-c <yannikkwc@gmail.com>
2022-05-23 06:47:09 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# SchoolPlanner
+# <b>SCHOOL PLANNER</b>
+## <b>Overview</b>
+This is  a web application that allows school administrators to easily manage their schedules.
+<br>
+![Website overview](./core/static/images/overview.png)
+
+
+
+## **Some features** 
+
+- Authentication 
+
+- Add, modify, delete a class,
+- Add a teacher and his schedule
+- Associate subjects to a teacher or a class
+- Associate classes to rooms
+- Generate the timetable according to a teacher, a class, a room
+- Graphical mode with drag and drop to easily position classes at selected times on the timetable
+
+
+
+## Some pictures
+
+![Website overview](./screenshots/overview.png)
+
+![Website overview](./screenshots/overview.png)
+
+
+## Want to use this project?
+
+1. Clone the repositiory into your workstation (device/machine) by running the command: <br>
+
+        git clone https://github.com/INF-L3-Project/SchoolPlanner-backend.git
+
+
+2. Create and activate a virtual environment:
+
+   ```sh
+   $ python3 -m venv venv && source venv/bin/activate
+   ```
+
+3. Install the requirements:
+
+   ```sh
+   (venv)$ pip install -r requirements.txt
+   ```
+
+4. Apply the migrations:
+
+   ```sh
+   (venv)$ python manage.py migrate
+   ```
+6. Inside `schoolplanner/` create a file name `.env` and use the example file in `env_example` to fill it file
+
+5. Seed the database:
+
+   ```sh
+   (venv)$ python manage.py migrate
+   ```
+
+6. Run the server:
+
+   ```sh
+   (venv)$ python manage.py runserver
+   ```
+
+
+7. Check the server is running by going to [localhost:8000](http://127.0.0.1:8000)
---
+++ schoolplanner/settings.py
@@ -0,0 +1,132 @@
+from pathlib import Path
+import os
+import environ  # Initialise environment variables
+import django_heroku
+
+env = environ.Env()
+environ.Env.read_env()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-w2z@5pcba613@lk8__fet)=jn3u3i)6nwdtgs^9^u4mkr@ddkm'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'core',
+    'authentication',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'schoolplanner.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'schoolplanner.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': '5432',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+LANGUAGE_CODE = 'fr'
+
+TIME_ZONE = 'Africa/Douala'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
