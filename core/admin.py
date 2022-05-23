e819022a92ac9bc39bb9c82f7044cb3bda9d04ee
Edghi <ghislain.makolle@facsciences-uy1.cm>
2022-05-23 14:45:36 +0100

    Models of the second module done

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from core.models import Field, Grade, Group, Level
 
-# Register your models here.
+
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ("name", "capacity", "level", "field")
+    list_search = ("name",)
+    list_filter = ("level__name", "field__name")
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("name", "capacity", "grade")
+    list_search = ("name",)
+    list_filter = ("grade__name",)
+
+
+admin.site.register(Field)
+admin.site.register(Grade, GradeAdmin)
+admin.site.register(Level)
+admin.site.register(Group, GroupAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Field(models.Model):
+    name = models.CharField(max_length=100, null=False, unique=True)
+    abr = models.CharField(max_length=100, null=False, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Level(models.Model):
+    name = models.CharField(max_length=100, null=False, unique=True)
+    abr = models.CharField(max_length=100, null=False, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Grade(models.Model):
+    name = models.CharField(
+        max_length=100,
+        null=False,
+        unique=True,
+    )
+    capacity = models.IntegerField(null=False)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+    field = models.ForeignKey(Field, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.field.abr}-{self.level.abr}"
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=100, null=False, unique=True)
+    capacity = models.IntegerField(null=False)
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.grade.name}-{self.name}"
