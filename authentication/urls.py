d11c9d17397ddf5015fd03c7b6538024f765b858
jugalux <jugalux111@gmail.com>
2022-06-08 15:28:44 +0100

    add active sidebar js code

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from authentication.views import LoginView, RegisterView, Logout
+from authentication.views import LoginView, RegisterView, Logout, IndexView
 
 app_name = 'authentication'
 urlpatterns = [
+    path('home/', IndexView.as_view(), name='index'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', Logout.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
